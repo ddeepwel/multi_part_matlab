20ad45cd119a270b2dd420e8c1027d033d3f1e1b
David Deepwell <ddeepwel@uwaterloo.ca>
2020-09-17 17:59:57 -0400

    Add particle settling functions

---
+++ analysis/particle_settling.m
@@ -0,0 +1,24 @@
+function [time, y_p, vel] = particle_settling(particle_ID)
+% get the settling velocity and depth of a particle
+
+if nargin == 0
+    particle_ID = 0;
+end
+
+% Read data
+try
+    file_name = sprintf('mobile_%d',particle_ID);
+    particle_data = check_read_dat(file_name);
+catch
+    file_name = sprintf('mobile');
+    particle_data = check_read_dat(file_name);
+end
+
+% get data
+time = particle_data.time;
+y_p  = particle_data.y;
+
+% calculate velocity
+Dmat = FiniteDiff(time, 1, 2, true, false); % some cases have variable time step 
+vel = Dmat * y_p;
+
---
+++ plotting/plot_particle_settling.m
@@ -0,0 +1,31 @@
+function [] = plot_particle_settling(particle_ID)
+% plot the settling of a single particle
+
+if nargin == 0
+    particle_ID = 0;
+end
+
+params = read_params();
+
+[time, y_p, vel] = particle_settling(particle_ID);
+
+% make figure
+figure(65)
+%clf
+subplot(2,1,1)
+hold on
+plot(time, y_p,'-')
+fprintf('Initial particle position: %g D_p\n',y_p(1))
+ylabel('$h_p/D_p$')
+title('particle height')
+grid on
+
+subplot(2,1,2)
+hold on
+plot(time, vel,'-')
+ylabel('$w_p/w_s$')
+xlabel('$t/\tau$')
+title('particle velocity')
+grid on
+
+figure_defaults()
