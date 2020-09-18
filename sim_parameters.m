4b0dc3740094a71b58435f703e56ba0068a024e0
David Deepwell <ddeepwel@uwaterloo.ca>
2020-09-18 16:42:00 -0600

    Update sim_parameters

--- sim_parameters.m
+++ sim_parameters.m
@@ -1,4 +1,4 @@
-function parms = sim_parameters(Re, Np, s, Fr, Dp_dx, ep)
+function parms = sim_parameters(Re, Np, s, Fr, Dp_dx, ep, pert_type)
 % create list of parameters to keep fixed and functions to get the correct varied parameters
 % Re: Reynolds number
 % Np: Number of particles
@@ -6,6 +6,12 @@ function parms = sim_parameters(Re, Np, s, Fr, Dp_dx, ep)
 % Fr: Froude number
 % Dp_dx: points per particle diameter
 % ep: epsilon, small parameter for perturbing particle location
+% pert_type: the type of perturbation to use
+%       - random: particles are randomly place anywhere along the z=0 line
+%                   without overlapping and with vertical perturbation of ep
+%       - xpert:  particles are perturbed horizontaly by amount on the order ep
+%       - ypert:  particles are perturbed vertically  by amount on the order ep
+%       - xypert: particles are perturbed horizontally and vertically  by amount on the order ep
 
 
 if nargin < 4
@@ -15,6 +21,9 @@ elseif nargin == 4
     perturb = false;
 elseif nargin == 5
     perturb = false;
+elseif nargin == 6
+    perturb = true;
+    pert_type = 'random';
 else
     perturb = true;
 end
@@ -50,21 +59,36 @@ Nx = Lx * Dp_dx;
 Ny = Ly * Dp_dx;
 Nz = Lz * Dp_dx;
 
-% particle locations
-xp = (-Lx/2+(s+1)/2):(s+1):(Lx/2-(s+1)/2);
-yp = 0* xp;
-
 if perturb
-    % probably need to look into a seed here!
-    xpert = normrnd(0,ep,[1,Np]);
-    ypert = normrnd(0,ep,[1,Np]);
+    rng('shuffle') % use the time to seed the random number generator
+    switch pert_type
+        case 'random'
+            for nn = 1:Np
+                xp_new = Lx*rand() - Lx/2;
+                if nn ~= 1
+                    while min(abs(xp - xp_new)) <= 1 || min(abs(Lx+xp - xp_new)) <= 1
+                        xp_new = Lx*rand() - Lx/2;
+                    end
+                end
+                xp(nn) = xp_new;
+            end
+            yp = normrnd(0,ep,[1,Np]);
 
-    %xp = xp + xpert;
-    %yp = yp + ypert;
+        case 'xpert'
+            xp = (-Lx/2+(s+1)/2):(s+1):(Lx/2-(s+1)/2);
+            xp = xp + normrnd(0,ep,[1,Np]);
+            yp = 0 * xp;
 
-    xp = xp + xpert.*[0 1 0];
-    %yp = yp + ypert;
+        case 'ypert'
+            xp = (-Lx/2+(s+1)/2):(s+1):(Lx/2-(s+1)/2);
+            yp = normrnd(0,ep,[1,Np]);
 
+        case 'xypert'
+            xp = (-Lx/2+(s+1)/2):(s+1):(Lx/2-(s+1)/2);
+            xp = xp + normrnd(0,ep,[1,Np]);
+            yp = normrnd(0,ep,[1,Np]);
+
+    end
 end
 
 parms.rho_0 = rho_0;
@@ -76,6 +100,7 @@ parms.gtilde= gtilde;
 parms.Ri    = Ri;
 parms.Pe    = Pe;
 parms.xp    = xp;
+parms.yp    = yp;
 
 fprintf('xmin    = %0.4g\n', -Lx/2);
 fprintf('xmax    = %0.4g\n',  Lx/2);
@@ -92,6 +117,13 @@ fprintf('grav    = %0.4g\n', gtilde);
 fprintf('Pe      = %0.4g\n', Pe);
 fprintf('Ri      = %0.5g\n', Ri);
 fprintf('particle positions:\n');
-for nn = 1:length(xp)
-    fprintf('  P%d:    %6.5g %6.5g\n', nn, xp(nn), yp(nn));
+for nn = 1:Np
+    fprintf('%6.5g %6.5g %6.5g %6.5g\n', xp(nn), yp(nn), 0, 0.5);
 end
+
+figure(301)
+clf
+viscircles([xp; yp]', ones(Np,1)*0.5);
+axis image
+xlim([-1 1]*Lx/2)
+figure_defaults()
