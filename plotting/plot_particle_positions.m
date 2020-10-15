0ae3a292d52822765d7bd8640947a8d292e2d5c7
David Deepwell <ddeepwel@uwaterloo.ca>
2020-10-15 08:44:53 -0700

    Update plot of particle positions and separation

---
+++ plotting/plot_particle_positions.m
@@ -0,0 +1,113 @@
+function [] = plot_particle_positions_xy(style)
+% plot particle positions and colour by time
+% styles are:
+%       'init'      - use initial particle positions
+%       'regular'   - assume that the particles are equispaced (or close to it)
+
+par = read_params();
+
+if nargin == 0
+    style = 'init';
+end
+
+
+% search for particle files
+particle_files = dir('mobile_*.dat');
+Np = length(particle_files);
+
+% read particle files
+p = cell(1, Np);
+for mm = 1:Np
+    fname = sprintf('mobile_%d', mm-1);
+    p{mm} = check_read_dat(fname);
+end
+
+% make plot
+figure(66)
+clf
+hold on
+
+time = p{1}.time;
+Nt = length(time);
+colormap(parula(Nt))
+cmap = colormap(parula(Nt));
+for mm = 1:Np
+    x = p{mm}.x;
+    y = p{mm}.y;
+    col = p{mm}.time;
+    scatter(x,y,[],col,'fill','SizeData',8)
+end
+
+% add horizontal bands at intervals of 5 time units
+%for ts = 0:5:time(end)
+%    ind = nearest_index(time, ts);
+%    cmap_ind = round(ts/time(end) * (Nt-1) + 1);
+%    plot([p{1}.x(ind) p{2}.x(ind)],[p{1}.y(ind) p{2}.y(ind)], 'Color', cmap(cmap_ind,:))
+%end
+
+% find particle spacing
+for mm = 1:Np
+    xp(mm) = p{mm}.x(1);
+end
+xp_mean = round(mean(diff(xp)));
+xp_reg = (par.xmin + xp_mean/2):xp_mean:(par.xmax + xp_mean/2);
+
+% add vertical lines of initial particle positions
+for mm = 1:Np
+    switch style
+        case 'init'
+            plot([1 1]*p{mm}.x(1), [par.ymax par.ymin],'k','color',[0 0 0 0.3])
+        case 'regular'
+            plot([1 1]*xp_reg(mm), [par.ymax par.ymin],'k','color',[0 0 0 0.3])
+    end
+end
+
+cbar = colorbar;
+cbar.Label.String = '$t/\tau$';
+xlabel('$x/D_p$')
+ylabel('$y/D_p$')
+title('particle position')
+axis([par.xmin, par.xmax par.ymin par.ymax])
+
+figure_defaults()
+
+% save figure
+check_make_dir('figures')
+cd('figures')
+%print_figure('particle_positions','format','pdf','size',[6 4])
+cd('..')
+
+
+%%%%% 2nd Figure %%%%%
+
+figure(67)
+clf
+hold on
+
+for mm = 1:Np
+    x = p{mm}.x;
+    y = p{mm}.y;
+    col = p{mm}.time;
+    switch style
+        case 'init'
+            xdraw = x - x(1);
+        case 'regular'
+            xdraw = x-xp_reg(mm);
+    end
+    scatter(xdraw,y,[],col,'fill','SizeData',8)
+    text(xdraw(round(Nt*3/4)), y(round(Nt*3/4)), num2str(mm))
+end
+
+switch style
+    case 'init'
+        xlabel('$(x-x_0)/D_p$')
+    case 'regular'
+        xlabel('$(x-x_{reg})/D_p$')
+end
+ylabel('$y/D_p$')
+grid on
+cbar = colorbar;
+cbar.Label.String = '$t/\tau$';
+
+
+figure_defaults()
---
+++ plotting/plot_particle_separation_multi.m
@@ -0,0 +1,106 @@
+function p_h = plot_particle_separation(save_plot, multi_plot)
+% plot separation between multiple particles
+
+if nargin == 0
+    save_plot = true;
+    multi_plot = false;
+elseif nargin == 1
+    multi_plot = false;
+end
+
+par = read_params();
+xmax = par.xmax;
+xmin = par.xmin;
+Lx = xmax - xmin;
+
+% search for particle files
+particle_files = dir('mobile_*.dat');
+Np = length(particle_files);
+
+figure(68)
+if ~multi_plot
+    clf
+end
+hold on
+
+for mm = 1:Np
+    % find separation between different particles
+    % need to handle periodicity differently
+    if mm == Np
+        [time, sep, sep_vel] = particle_separation(mm-1, 0);
+        sep = Lx - sep;
+    else
+        [time, sep, sep_vel] = particle_separation(mm-1, mm);
+    end
+
+    % find number of outputs of first particle
+    if mm == 1
+        Nt = length(time);
+        Dmat = FiniteDiff(time,1,2,true,false);
+    end
+    sep_stacked(mm,:) = sep(1:Nt);
+
+    % check if reached bottom and don't plot after this
+    hit_bottom = reached_bottom();
+    if hit_bottom
+        [tb, ti] = reach_bottom_time;
+        inds = 1:ti;
+    else
+        inds = 1:length(time);
+    end
+
+    subplot(2,1,1)
+    hold on
+    plot(time(inds), sep(inds)-1) % assuming D_p = 1
+    if hit_bottom
+        plot(time(ti),sep(ti)-1,'kx')
+    end
+
+    if mm == Np
+        leg_lab{mm} = sprintf('$%d - %d$',mm,1);
+    else
+        leg_lab{mm} = sprintf('$%d - %d$',mm,mm+1);
+    end
+    text(time(round(Nt/2)), sep(round(Nt/2))-1, leg_lab{mm});
+
+    subplot(2,1,2)
+    hold on
+    vel = Dmat(1:Nt,1:Nt) * sep(1:Nt);
+    plot(time(1:Nt), vel)
+    text(time(round(Nt/2)), vel(round(Nt/2)), leg_lab{mm});
+end
+
+xlabel('$t/\tau$')
+ylabel('derivative of $s$')
+%yl = ylim();
+%ylim([0 yl(2)])
+grid on
+%legend(leg_lab)
+
+subplot(2,1,1)
+grid on
+ylabel('$(s-D_p)/D_p$')
+title('particle separation')
+
+figure_defaults()
+
+if save_plot
+    check_make_dir('figures')
+    cd('figures')
+    %print_figure('particle_separation','format','pdf','size',[6 4])
+    cd('..')
+end
+
+
+%%%%% 2nd plot %%%%%
+
+% mean particle separation
+xp_mean = round(mean(sep_stacked(:,1)));
+
+figure(70)
+clf
+area(time(1:Nt),(sep_stacked'-1));%/(xp_mean-1))
+xlabel('$t/\tau$')
+ylabel('$s/s_0$')
+
+figure_defaults()
