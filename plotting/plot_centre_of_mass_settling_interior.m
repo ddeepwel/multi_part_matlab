31d391cfd6f8367822ee27c79f5133c2abd00043
David Deepwell <ddeepwel@uwaterloo.ca>
2020-10-28 09:08:51 -0700

    Centre of mass of interior particles

---
+++ plotting/plot_centre_of_mass_settling_interior.m
@@ -0,0 +1,29 @@
+function p_h = plot_centre_of_mass_settling_interior(multi_plot)
+% plot the centre of mass of the interior particles over time
+% interior particles are all particles minus the left and right most
+
+if nargin == 0
+    multi_plot = false;
+end
+
+[time,x_com,y_com,z_com] = particle_centre_of_mass_interior();
+Dmat = FiniteDiff(time,1,2,true,false);
+v_com = Dmat * y_com;
+
+figure(85)
+if ~multi_plot
+    clf
+end
+
+subplot(2,1,1)
+hold on
+plot(time,y_com)
+ylabel('$y_\mathrm{COM}/D_p$','Interpreter','latex')
+
+subplot(2,1,2)
+hold on
+plot(time,v_com)
+xlabel('$t/\tau$')
+ylabel('$v_\mathrm{COM}/w_s$','Interpreter','latex')
+
+figure_defaults()
